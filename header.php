ae306a21f65770684348cf04a7602186a10f09ef
lili0allen <lili0allen@gmail.com>
2015-03-05 15:08:23 +1100

    hide language selection

--- header.php
+++ header.php
@@ -40,12 +40,45 @@
 
 <link type="text/css" rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/bxslider/jquery.bxslider.css"/>
 
-
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
+
+
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/falcontech.js"></script>
+
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootbox.js"></script>
+<script src="<?php echo get_stylesheet_directory_uri(); ?>/bxslider/jquery.bxslider.min.js"></script>
+<script>
+var lg=jQuery.noConflict();
+lg(document).ready(function(){
+	lg('.certified-logo').bxSlider({
+		slideWidth: 5000,
+		minSlides: 3,
+		maxSlides: 3,
+		slideMargin: 10,
+		pager: false,
+		onSliderLoad: function(){
+			lg(".certified-logo").css("visibility","visible");
+			lg(".certified-logo").css("height","auto");
+		  }
+	  });
+	lg('.partners-logo').bxSlider({
+		slideWidth: 5000,
+		minSlides: 3,
+		maxSlides: 3,
+		slideMargin: 10,
+		pager: false,
+		onSliderLoad: function(){
+			lg(".partners-logo").css("visibility","visible");
+			lg(".partners-logo").css("height","auto");
+		  }
+	  });
+});
+				</script>
 </head>
 
 <body <?php body_class("clearfix"); ?>>
@@ -53,7 +86,7 @@
         <div class="row dark-gray-bg">
             <div class="header container ">
                 <div class="col-md-12 header-top">
-                    <span>Change Langage? <a href="#">Englsh</a> | <a href="#">中文</a></span>    
+                    <!--<span>Change Langage? <a href="#">Englsh</a> | <a href="#">中文</a></span>-->    
                 </div>
             </div>
         </div>
@@ -69,7 +102,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
               </button>
-              <a class="navbar-brand" href="<?php echo bloginfo('url'); ?>">Falcon Tech </a>
+              <a class="navbar-brand" href="<?php echo bloginfo('url'); ?>">FalconTech </a>
             </div>
             <div class="pull-right">
                 <form class="navbar-form navbar-left" role="search" method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
@@ -80,7 +113,7 @@
               </form>
               <span class="about-links">
                   <ul>
-                      <li><a href="#">about falcon tech</a></li> | <li><a href="#">contact</a></li>
+                      <li><a href="<?php echo bloginfo('url'); ?>/about">about FalconTech</a></li> | <li><a href="<?php echo bloginfo('url'); ?>/contact">contact</a></li>
                   </ul>
               </span>
             </div>
